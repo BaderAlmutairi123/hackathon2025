d90200b652e594e6aed6001d3457d6150672b011
RaytheCray <raybhatti19@gmail.com>
2025-04-12 00:07:03 -0400

    Logo and name update

--- Homer/src/app.tsx
+++ Homer/src/app.tsx
@@ -12,19 +12,19 @@ export function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
+          <img src={"https://avatars.githubusercontent.com/u/148819856?v=4"} class="logo" alt="Bader logo" />
         </a>
         <a href="https://preactjs.com" target="_blank">
           <img src={preactLogo} class="logo preact" alt="Preact logo" />
         </a>
       </div>
-      <h1 className={"text-blue-800 "}>Vite + Preact</h1>
+      <h1 className={""}>Homer</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
+          
         </p>
       </div>
       <p className="text-amber-800">
